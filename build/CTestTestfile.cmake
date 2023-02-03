5f502de66db25f88f39f5d4155d6114b97c0b325
Daidalos99 <daidalos0430@gmail.com>
2023-02-03 12:51:39 +0900

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/daidalos/catkin_ws/src/qs_bot
+# Build directory: /home/daidalos/catkin_ws/src/qs_bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
